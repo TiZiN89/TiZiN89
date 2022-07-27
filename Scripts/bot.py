5b05a01d8a36c26c324f63a6bf1ff58d757178b1
TiZiN89 <ASASSA@mail.ru>
2022-07-27 23:05:27 +0300

    bot1

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -10,32 +10,47 @@ bot = telebot.TeleBot(settings.TOKEN)
 def start(message):
     if message.text == "hi":
         bot.send_message(message.chat.id, "Fuck you!")  # посылает сообщение
-        murkup_inline = types.InlineKeyboardMarkup()  # делается клавиатура
-        item1 = types.InlineKeyboardButton(text='Да', callback_data='yes')  # первая кнопка
-        item2 = types.InlineKeyboardButton(text='нет', callback_data='no')  # вторая кнопка
-        murkup_inline.add(item1, item2)  #  кнопки добавляются в клавиатуру
-        bot.send_message(message.chat.id, 'Do you wanna send me in the same way?', reply_markup=murkup_inline)
+        first_level_button(message)
+
     else:
         bot.send_message(message.chat.id, "Try to print 'hi' , asshole!!!")
 
+def first_level_button(message):
+    murkup_inline = types.InlineKeyboardMarkup()  # делается клавиатура
+    item1 = types.InlineKeyboardButton(text='Да', callback_data='yes')  # первая кнопка
+    item2 = types.InlineKeyboardButton(text='нет', callback_data='no')  # вторая кнопка
+    murkup_inline.add(item1, item2)  # кнопки добавляются в клавиатуру
+    bot.send_message(message.chat.id, 'Do you wanna send me in the same way?', reply_markup=murkup_inline)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def second_level(call):
+@bot.callback_query_handler(func=lambda call: call.data == "yes")
+def second_level_yes(call):
     if call.data == 'yes':  # callback_data c функции выше
-        murkup_reply = types.InlineKeyboardMarkup()
-        item1 = types.InlineKeyboardButton(text='First chance', callback_data='one')
-        item2 = types.InlineKeyboardButton(text='Second chance', callback_data='to')
-        murkup_reply.add(item1, item2)
-        bot.send_message(call.message.chat.id, 'Choice your destiny:', reply_markup=murkup_reply)
+        second_level_button(call)
+
+@bot.callback_query_handler(func=lambda call: call.data == "no")
+def second_level_no(call):
     if call.data == 'no':
         bot.send_message(call.message.chat.id, 'Good for you!!!')
         bot.answer_callback_query(callback_query_id=call.id, text="Good choice, mate.")
 
-@bot.callback_query_handler(func=lambda call: True)
-def second_answer(call):
-    if call.data == 'First chance':
-        bot.send_message(call.message.chat.id, "ooxdfhzdf")
+def second_level_button(call):
+    murkup_reply = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(text='First chance', callback_data='es')
+    item2 = types.InlineKeyboardButton(text='Second chance', callback_data='o')
+    murkup_reply.add(item1, item2)
+    bot.send_message(call.message.chat.id, 'Choice your destiny:', reply_markup=murkup_reply)
+    return True
+
+@bot.callback_query_handler(func=lambda call: call.data == 'es')
+def second_answer1(call):
+    if call.data == 'es':
+        bot.send_message(call.message.chat.id, "Fuck you again!!!))))")
+
+@bot.callback_query_handler(func=lambda call: call.data == 'o')
+def second_answer2(call):
+    if call.data == 'o':
+        bot.send_message(call.message.chat.id, 'You are shrimp')
 
 
 bot.polling(none_stop=True)
