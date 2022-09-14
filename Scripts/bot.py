b7b80507a5f8b80e8164b2a9041b63d0d3bcd9f5
TiZiN89 <ASASSA@mail.ru>
2022-09-14 11:21:22 +0300

    bd

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -1,27 +1,25 @@
-create table teacher (
-teacher_id int (4) AUTO_INCREMENT,
+ccreate table teacher (
 Name_FIO varchar (50) NOT NULL,
-PRIMARY KEY (teacher_id)
+PRIMARY KEY (Name_FIO)
 );
 
 create table room (
-room_id int (4) AUTO_INCREMENT,
 room_number int (4) NOT NULL,
-PRIMARY KEY (room_id)
+PRIMARY KEY (room_number)
 );
 
 create table student_group (
-group_id int (4) AUOT_INCREMENT,
 group_number int (4) NOT NULL,
-PRIMARY KEY (group_ID)
+PRIMARY KEY (group_number)
 );
 
 create table reservation (
+res_id int (4) AUTO INCREMENT,
 time int (4) NOT NULL,
 res_teacher varchar (50),
 res_room int (4),
 res_groop int (4),
-PRIMARY KEY (time, res_room),
+PRIMARY KEY (res_id),
 FOREIGN KEY (res_teacher) REFERENCES teacher (teacher_id),
 FOREIGN KEY (res_room) REFERENCES room (room_id),
 FOREIGN KEY (res_group) REFERENCES student_group (groop_id)
