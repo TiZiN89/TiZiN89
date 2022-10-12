526f5d5b44be895442f780932728de0cf7e3699a
TiZiN89 <ASASSA@mail.ru>
2022-10-12 12:38:20 +0300

    bd2

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -1,28 +1,47 @@
-create table teacher (
-teacher_id int (4) AUTO INCREMENT,
-Name_FIO varchar (50) NOT NULL,
-PRIMARY KEY (teacher_id)
+create table profile (
+user_id number (4) AUTO_INCREMENT,
+birthday number (6) NOT NULL,
+F.I.O varchar (30) NOT NULL,
+sex (1) NOT NULL
+PRIMARY KEY (user_id)
 );
 
-create table room (
-room_number int (4) NOT NULL,
-PRIMARY KEY (room_number)
+create table attribute (
+user_id number(4) NOT NULL,
+attribute_id number (4) AUYO_INCREMENT,
+autoCreate (1),										???
+type number(2) NOT NULL,							???
+description vrchar (50),
+PRIMARY KEY (attribute_id),
+unique (user_id, type),
+FOREIGN KEY (user_id) PEFERENSES profile (user_id)
 );
 
-create table student_group (
-group_number int (4) NOT NULL,
-PRIMARY KEY (group_number)
+create teble attibute-view (
+ancher number(4) NOT NULL,
+attribute_id number (4) NOT NULL,
+view_id number (4) AUTO_INCREMENT,
+image number (2),									???
+PRIMARY KEY (view_id),
+unique (attribute_id, arher)
+FORIGN KEY (attribute-id) REFERENSES attribute (attribute_id)
 );
 
-create table reservation (
-res_id int (4) AUTO INCREMENT,
-time int (4) NOT NULL,
-res_teacher varchar (50),
-res_room int (4) NOT NULL,
-res_group int (4) NOT NULL,
-PRIMARY KEY (res_id),
-UNIQUE (time, res_room)
-FOREIGN KEY (res_teacher) REFERENCES teacher (teacher_id),
-FOREIGN KEY (res_room) REFERENCES room (room_id),
-FOREIGN KEY (res_group) REFERENCES student_group (group_number)
+create table view (
+view_id number (4) AUTO_INCREMENT,
+PRIMARY KEY (view_id),
+FOREIGN KEY (view_id) REFERENSES attribute-view ( view_id)
+);
+
+create table data (
+user_id number (4) AUTO_INCREMENT,
+attribute_id number (4) NOT NULL,
+view_id number (4) NOT NULL,
+date number (4) NOT NULL,
+value verchar (50) NOT NULL,
+PRIMARY KEY (date)
+unique (user_id, view_id)
+FOREIGN KEY (user_id) REFERENSES profile (user_id),
+FOREIGN KEY (view_id) REFERENSES view ( view_id),
+FOREIGN KEY (attribute_id) REFERENSES attribute (attribute_id)
 );
